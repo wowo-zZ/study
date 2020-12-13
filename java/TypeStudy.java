08fa36b9bd73199ef48d26ddacc815a9a25a920f
wowo_zZ <zhenggui5228@126.com>
2020-12-13 22:19:13 +0800

    java-2

---
+++ java/TypeStudy.java
@@ -0,0 +1,31 @@
+public class TypeStudy {
+
+    private static final int CONST_VALUE = 12345;
+    
+    public static void main(String[] args) {
+        System.out.println(TypeStudy.testUpperAndLowerCase());
+        TypeStudy.testComment();
+        final int b = 1234;
+        System.out.println(1.1/0);
+        // System.out.println(1/0);
+        System.out.println(2 >> 1);
+        System.out.println(2 >> 1);
+    }
+
+    private static boolean testUpperAndLowerCase() {
+        int bigNumber = 1000;
+        int BigNumber = 10000;
+        return bigNumber == BigNumber;
+    }
+
+    private static void testComment() {
+        // this is a line comment;
+        /*
+        this is multi-lines comments;
+        */
+        /**
+         * this is a document comment;
+         */
+    }
+
+}
