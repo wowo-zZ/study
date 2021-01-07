8453ebf11ee6e23c1e8c6a49f2ffe698bec9d807
wowo-zZ <zhenggui5228@126.com>
2021-01-07 10:47:17 +0800

    TestInteger

---
+++ java/TestInteger.java
@@ -0,0 +1,10 @@
+import java.lang.Integer;
+
+public class TestInteger {
+    
+    public static void main(String... args) {
+        Integer i = Integer.valueOf(1);
+        System.out.println(i);
+        System.out.println(i.getClass().getName());
+    }
+}
