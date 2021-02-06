c8fbcc24aa66fa5057b7ebbb3923cc844f4f2d89
wowo_zZ <zhenggui5228@126.com>
2021-02-06 16:37:24 +0800

    qualifier

--- java/spring/src/main/java/com/wowozz/study/Components/Sword.java
+++ java/spring/src/main/java/com/wowozz/study/Components/Sword.java
@@ -1,6 +1,7 @@
 package com.wowozz.study.Components;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
@@ -14,7 +15,8 @@ public class Sword {
     @Autowired
     Environment env;
 
-    @Autowired
+    @Autowired()
+    @Qualifier("benz")
     Car car;
     
     public void cut()
--- java/spring/src/main/java/com/wowozz/study/Components/impl/Audi.java
+++ java/spring/src/main/java/com/wowozz/study/Components/impl/Audi.java
@@ -2,7 +2,6 @@ package com.wowozz.study.Components.impl;
 
 import com.wowozz.study.Components.Car;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
@@ -10,10 +9,9 @@ import org.springframework.stereotype.Component;
 @Component
 @Profile("prod")
 @Primary
-@Qualifier("benz")
-public class Benz implements Car{
+public class Audi implements Car{
     
     public void run() {
-        System.out.println("this is benz");
+        System.out.println("this is audi");
     }
 }
